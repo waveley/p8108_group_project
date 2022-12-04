6fa1a3bf2d86c63b10dda59cc27923baebf42c74
yunyij <95501296+yunyij@users.noreply.github.com>
2022-12-03 21:11:54 -0500

    code

---
+++ code_yj.R
@@ -0,0 +1,15 @@
+## Models -- Checking PH Assumptions
+<center>
+  ![](img1.JPG){width=105%}
+</center>
+  
+## Models -- Fitting Exponential and Weibull Models
+<center>
+  ![](img2.JPG){width=105%}
+</center>
+  
+## Models -- Graphical Assessment of Parametric Model Assumptions using KM estimator
+<center>
+  ![Plots are not linear/slope is not 1](img3.JPG)
+</center>
+  
